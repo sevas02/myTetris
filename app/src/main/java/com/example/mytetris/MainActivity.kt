7e7cf85d679f26bebf8c3e333937310674b1d2e1
sevas02 <vsevolod.sum02@gmail.com>
2024-11-06 18:52:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mytetris/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mytetris
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mytetris.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        supportActionBar?.hide()
+
+        val btnExit = binding.bntExit
+        btnExit.setOnClickListener {
+            finish()
+        }
+
+    }
+}
